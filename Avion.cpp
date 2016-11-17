cf9913f639033f9c6f3710d6ee0313c07a4ac07d
Wolfy76700 <wolfy76700@yahoo.fr>
2016-11-17 10:10:52 +0100

    Méthode Avion #1

--- Avion.cpp
+++ Avion.cpp
@@ -1,4 +1,5 @@
 #include "Avion.h"
+#include<osg/vec3f>
 
 Avion::Avion(){
 	up=osg::Vec3f(0.0,0.0,1.0);
@@ -10,3 +11,5 @@ Avion::Avion(){
 
 Avion::~Avion(){
 }
+
+virtual void Avion::avancer(int cube_size,&vector<avion*> avions, int i){}
