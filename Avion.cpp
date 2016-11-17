05963a99e6d86bc1d8b427feaa268f8a20002671
Helluys <luys@live.fr>
2016-11-17 10:50:11 +0100

    Update Avion.cpp
    Méthode Avion::tourner

--- Avion.cpp
+++ Avion.cpp
@@ -1,5 +1,6 @@
 #include "Avion.h"
-#include<osg/vec3f>
+#include <osg/vec3f>
+#include <osg/Geometry>
 
 Avion::Avion(){
 	up=osg::Vec3f(0.0,0.0,1.0);
@@ -13,3 +14,14 @@ Avion::~Avion(){
 }
 
 void Avion::avancer(int cube_size,&vector<avion*> avions, int i){};
+
+void Avion::tourner (float tang, float dir)
+{
+	osg::Quat q1 = osg::Quat( tang*osg::PI/180.f,osg::Vec3d(direction^up),0, osg::Vec3d(direction),0, osg::Vec3d(up)); 
+	up=q1*osg::Vec3d(up);
+
+	osg::Quat q2 = osg::Quat( 0, osg::Vec3d(direction^up),dir*osg::PI/180.f, osg::Vec3d(direction),0, osg::Vec3d(up));
+	direction=q2*q1*osg::Vec3d(direction);
+
+	up=q2*osg::Vec3d(up);
+}
