41ad542fe4a9da343c500a35d6def6af715d1298
gjrvb114 <gjrvb114@gmail.com>
2016-11-17 11:03:24 +0100

    Update Ennemi.cpp

--- Ennemi.cpp
+++ Ennemi.cpp
@@ -35,3 +35,34 @@ void Ennemi::avancer(int cube_size):avion(int cube_size){
         position.z -= taille;
     }
 }
+
+void avionEnnemi::Strategie(std::vector<Avion> &avions){
+
+	//Trouver ennemi le plus proche
+	Avion *plusProche;
+	plusProche=trouverJoueurProche(avions);+
+
+	//
+
+//Verifier murs
+
+}
+
+avionEnnemi::~avionEnnemi(){
+}
+
+Avion* trouverJoueurProche(std::vector<Avion> avions){
+	int i,j=0;
+	for(i=1;i<avions->size();i++)
+	{
+		if(dist(this->position,avions[i].position)<dist(this->position,avions[j].position))
+		{
+
+		}
+	}
+}
+
+float dist(osg::Vec3f a,osg::Vec3f b){
+	return (std::sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y)+(a.z-b.z)*(a.z-b.z)));
+}
+}
