fd3aa243bc002a81dd0997c83bb8200b58f2a385
franckvalentini <franck.valentini@eleves.ec-nantes.fr>
2016-11-17 10:05:51 +0100

    Update Ennemi.cpp

--- Ennemi.cpp
+++ Ennemi.cpp
@@ -1,1 +1,11 @@
 #include "Ennemi.h"
+
+avionEnnemi::avionEnnemi(osg::Vec3f pos, osg::Vec3f dir) : Avion()
+{
+	position = pos;
+	direction = dir;
+	camp=false;
+}
+
+avionEnnemi::~avionEnnemi(){
+}
